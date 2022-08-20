712c5d9c90d70dfedc1cd54af993135440289d3c
maxmfernan <maxmfernan@gmail.com>
2022-08-20 19:00:05 -0300

    Adicionei curso sobre quarkus. Parei no quarto vídeo.

---
+++ Quarkus/bitcoin/src/main/java/br/com/alura/model/Post.java
@@ -0,0 +1,8 @@
+package br.com.alura.model;
+
+public class Post {
+    private Long id;
+    private long userId;
+    private String title;
+    private String body;
+}
---
+++ Quarkus/bitcoin/src/main/java/br/com/alura/model/User.java
@@ -0,0 +1,6 @@
+package br.com.alura.model;
+
+public class User {
+    private Long id;
+    private String username;
+}
