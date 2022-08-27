130ab626b17c182cd6926153ff96e449609ba490
maxmfernan <maxmfernan@gmail.com>
2022-08-27 16:32:05 -0300

    Estou estudando o comportamento do objeto do tipo Response

--- Quarkus/bitcoin/src/main/java/br/com/alura/resource/BitcoinResource.java
+++ Quarkus/bitcoin/src/main/java/br/com/alura/resource/BitcoinResource.java
@@ -3,12 +3,15 @@ package br.com.alura.resource;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.json.Json;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.Produces;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.logging.Logger;
 
 import br.com.alura.model.Post;
 import br.com.alura.service.BitcoinService;
@@ -20,10 +23,17 @@ public class BitcoinResource {
     @RestClient
     BitcoinService bitcoinService;
 
+    @Inject
+    Logger log;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Post> list(){
-        return bitcoinService.list();
+    public Response list(){
+        log.infof("Chamando a API consumida.");
+        Response response = bitcoinService.list();
+        log.infof("Resposta tem entidade?: " + response.hasEntity());
+        log.infof("Resposta stringficada: " + response.readEntity(String.class));
+        return response;
     }
 
 }
--- Quarkus/bitcoin/src/main/java/br/com/alura/service/BitcoinService.java
+++ Quarkus/bitcoin/src/main/java/br/com/alura/service/BitcoinService.java
@@ -7,6 +7,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
@@ -18,6 +19,6 @@ public interface BitcoinService {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Post> list();
+    public Response list();
     
 }
